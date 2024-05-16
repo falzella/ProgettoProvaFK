4a74959e8c40557d4fcbcd8f44ae91356fe8c080
Falz <falzelolloso@gmail.com>
2024-05-16 21:53:36 +0200

    base upload funziona

---
+++ ApplicativoProgetto/src/main/java/javaDB/FileUploadServlet.java
@@ -0,0 +1,27 @@
+package javaDB;
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+/* The Java file upload Servlet example */
+
+@WebServlet(name = "FileUploadServlet", urlPatterns = { "/fileuploadservlet" })
+@MultipartConfig(
+        fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
+        maxFileSize = 1024 * 1024 * 10,      // 10 MB
+        maxRequestSize = 1024 * 1024 * 100   // 100 MB
+)
+public class FileUploadServlet extends HttpServlet {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        /* Receive file uploaded to the Servlet from the HTML5 form */
+        Part filePart = request.getPart("file");
+        String fileName = filePart.getSubmittedFileName();
+        for (Part part : request.getParts()) {
+            part.write("C:/Users/Big Chungus/Desktop/kami/" + fileName);
+        }
+        response.getWriter().print("The file uploaded sucessfully.");
+    }
+
+}
--- ApplicativoProgetto/src/main/webapp/addimage.jsp
+++ ApplicativoProgetto/src/main/webapp/addimage.jsp
@@ -1,5 +1,17 @@
-<form action="provainserisciimmagine.jsp" method="post" enctype="multipart/form-data">
-    Select File:<input type="file" name="fname"/><br/>
-    <input type="image" src="imagetree/profilepic/11.jpg"/>
-    <input type ="submit"> Manda
+<%@page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@include file="connessione.jsp"%>
+<%@include file="getidevento.jsp"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Java File Upload Servlet Example </title>
+</head>
+<body>
+
+<form method="post" action="fileuploadservlet" enctype="multipart/form-data">
+    <input type="file" name="file" />
+    <input type="submit" value="Upload" />
 </form>
+
+</body>
+</html>
